55940b465cad37be43cc0b7382e80a67a2e479e6
Lenovo <tanzhaoqi47@gmail.com>
2026-04-10 10:20:53 +0800

    FALSH模块

---
+++ BSP/iap.c
@@ -0,0 +1,9 @@
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "stmflash.h"
+#include "iap.h"
+
+
+
+
---
+++ BSP/iap.h
@@ -0,0 +1,18 @@
+#ifndef __IAP_H__
+#define __IAP_H__
+#include "sys.h"  
+
+
+typedef  void (*iapfun)(void);	
+
+#define FLASH_APP1_ADDR		0x0800F000  
+
+
+
+
+
+
+#endif
+
+
+
